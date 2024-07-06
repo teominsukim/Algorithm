4bf03dd9ffeb9d6e431cb54526be2a5e4fa2b02b
teominsukim <2024112777@dgu.ac.kr>
2024-07-07 05:15:09 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 1116 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.c
+++ 백준/Bronze/15829. Hashing/Hashing.c
@@ -7,16 +7,20 @@ int main(){
   int n;
   scanf("%d",&n);
   getchar();
-  char *arr = (char*)malloc(sizeof(int)*n);
+  char *arr = (char*)malloc(sizeof(char)*(n+1));
   long long result = 0;
   
   for(int i =0; i<n; i++)
     scanf("%c",&arr[i]);
+  long long pow = 1;
   
   for(int i=0; i<n; i++){
-    int plus = (pow(31,i)*(arr[i]+1-'a'));
-    result += plus % 1234567892;
+    
+    long long plus = pow*(arr[i]+1-'a') % 1234567891;
+    result = (result + plus) % 1234567891;
+    pow = pow * 31 % 1234567891;
   }
   
   printf("%lld",result);
+  free(arr);
 }
