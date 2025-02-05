b830333ea35b4a82c7a37ba961346b769e44b853
김민수 <2024112777@dgu.ac.kr>
2025-02-05 21:55:27 +0900

    [Silver V] Title: 올림픽, Time: 0 ms, Memory: 2024 KB, Score: 20 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.cc
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+struct country
+{
+    int id;
+    int gold;
+    int silver;
+    int bronze;
+    int rank;
+};
+
+bool compare(country a, country b)
+{
+    if (a.gold == b.gold)
+    {
+        if (a.silver == b.silver)
+        {
+            return a.bronze > b.bronze;
+        }
+        else
+        {
+            return a.silver > b.silver;
+        }
+    }
+    else
+    {
+        return a.gold > b.gold;
+    }
+}
+
+int solve()
+{
+
+    int n, m;
+    cin >> n >> m;
+    vector<country> countrys;
+    for (int i = 0; i < n; i++)
+    {
+        country input;
+        int id;
+        int gold;
+        int silver;
+        int bronze;
+        cin >> id;
+        cin >> gold;
+        cin >> silver;
+        cin >> bronze;
+        input.id = id;
+        input.gold = gold;
+        input.silver = silver;
+        input.bronze = bronze;
+        countrys.push_back(input);
+    }
+
+    sort(countrys.begin(), countrys.end(), compare);
+
+    int count = 0;
+    for (int j = 0; j < n; j++)
+    {
+        
+
+        count++;
+
+        if (j > 0 && (countrys[j].gold == countrys[j - 1].gold && countrys[j].bronze == countrys[j - 1].bronze && countrys[j].silver == countrys[j - 1].silver))
+        {
+            count--;
+        }
+
+        if (countrys[j].id == m)
+        {
+            return count;
+        }
+
+    }
+
+    return -1;
+}
+
+int main()
+{
+    cout << solve();
+}
